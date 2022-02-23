b6e3845cd7303c071a4cb70238c350606fcfde8c
Vincenzo Fasano <vfasano@hawk.iit.edu>
2022-02-23 13:06:32 -0800

    Add and format buttons

---
+++ billingPage/site.js
@@ -0,0 +1,11 @@
+document.getElementById("next").onclick = function () {
+  location.href = "../shippingPage/index.html";
+};
+
+document.getElementById("back").onclick = function () {
+  location.href = "../";
+};
+
+document.getElementById("home").onclick = function () {
+  location.href = "../";
+};
